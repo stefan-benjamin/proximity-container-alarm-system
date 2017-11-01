0bced35891c7ac5fad14c53f4fcc74a0b8204a7f
Stefan Bejan <stefan.bejan90@gmail.com>
2017-11-01 15:53:11 +0100

    Initial BLE beacon implementation on Raspi 3

--- SensorMonitorBLE/SensorMonitorBLE/app.js
+++ SensorMonitorBLE/SensorMonitorBLE/app.js
@@ -1,3 +1,33 @@
 'use strict';
 
 console.log('Hello world');
+
+var bleno = require('bleno');
+var SystemInfo = require('./systemInformationService');
+
+var systemInfoService = new SystemInfo();
+
+bleno.on('stateChange', function (state) {
+   console.log("BLE state: " + state);
+
+   if (bleno.state === "poweredOn")
+   {
+      console.log ( "Starting advertising..." )
+
+      bleno.startAdvertising("01234567891234567890123456789", systemInfoService.uuid, function(error) {
+         console.log("Error occured while starting advertising. " + error );
+      });
+   }
+});
+
+bleno.on('advertisingStart', function (error) {
+   console.log('on -> advertisingStart: ' +
+      (error ? 'error ' + error : 'success')
+   );
+   if (!error) {
+      bleno.setServices([
+         systemInfoService
+      ]);
+   }
+});
+
---
+++ SensorMonitorBLE/SensorMonitorBLE/systemInformationService.js
@@ -0,0 +1,16 @@
+﻿var bleno = require('bleno');
+var util = require('util');
+
+var UptimeCharacteristic = require('./upTimeCharacteristic');
+
+function SystemInformationService() {
+  bleno.PrimaryService.call(this, {
+    uuid: 'ff51b30e-d7e2-4d93-8842-a7c4a57dfb07',
+    characteristics: [
+       new UptimeCharacteristic()
+    ]
+  });
+};
+
+util.inherits(SystemInformationService, bleno.PrimaryService);
+module.exports = SystemInformationService;
