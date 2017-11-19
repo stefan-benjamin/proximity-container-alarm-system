18696ddd82a32ecb1693dd1bf4b5630d2f9fc164
Tomas Kulik <tomaskulik@icloud.com>
2017-11-19 16:15:15 +0100

    Added data access to the DB

--- ContainerServer/ContainerServer/DataAccessLayer.js
+++ ContainerServer/ContainerServer/DataAccessLayer.js
@@ -1,15 +1,35 @@
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('containerSystem.db', 'username', 'password', {
-    host: 'localhost',
-    dialect: 'sqlite',
+'use strict';
 
-    pool: {
-        max: 5,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
-    },
-
-    // SQLite only
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize('containerSystemDB', null, null, {
+    dialect: "sqlite",
     storage: './../../DBScripts/containerSystem.db'
 });
+
+sequelize.authenticate().then(function (err) {
+    console.log('Connection successful');
+}, function (err) {
+    console.log('Connection unsuccessful', err)
+});
+
+var Alarm = sequelize.define('ALARM',{
+   Id: {type: Sequelize.INTEGER, primaryKey: true, autoincrement: true },
+   Name: {type: Sequelize.STRING},
+   Description: {type: Sequelize.STRING},
+   Resolution: {type: Sequelize.STRING}},
+{
+    freezeTableName: true,
+    tableName: 'ALARM',
+    timestamps: false
+});
+//Create data
+//var alarm = Alarm.build({
+//    Name: 'Xtreme',
+//    Description : 'Xtreme',
+//    Resolution: 'Xtreme'
+//})
+
+//save data
+//alarm.save().then(() =>{
+
+//})
