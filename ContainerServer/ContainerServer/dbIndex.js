91f1992c18b2bd5b61926169fc09e6f83241af5e
Tomas Kulik <tomaskulik@icloud.com>
2017-11-25 18:33:45 +0100

    removed dbIndex in favor of model index...

--- ContainerServer/ContainerServer/dbIndex.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var Sequelize = require('sequelize');
-var sequelize = new Sequelize('containerSystemDB', null, null, {
-    dialect: "sqlite",
-    storage: './../../DBScripts/containerSystem.db'
-});
